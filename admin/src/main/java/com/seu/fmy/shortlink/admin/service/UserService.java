f0f91f50e78dfaad96736bca42e5a103a1a5b1fc
dream_Flying <84890202+fmy-fly@users.noreply.github.com>
2025-10-02 20:17:01 +0800

    开发短链接后管系统用户注册相关功能（含redis布隆过滤器删选用户名）

--- admin/src/main/java/com/seu/fmy/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/seu/fmy/shortlink/admin/service/UserService.java
@@ -3,8 +3,12 @@ package com.seu.fmy.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.seu.fmy.shortlink.admin.dao.entity.UserDO;
+import com.seu.fmy.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.seu.fmy.shortlink.admin.dto.resp.UserRespDTO;
 
+/**
+ * 用户接口层
+ */
 /**
  * 用户接口层
  */
@@ -17,4 +21,20 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否存在
+     *
+     * @param username 用户名
+     * @return 用户名存在返回 True，不存在返回 False
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     *
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
+
