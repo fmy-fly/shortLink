40f678252127770dd42d8562ccb39fe208068a61
fmy <1791477899@qq.com>
2025-10-10 16:57:24 +0800

    开发短链接后管系统通用持久层实现

---
+++ admin/src/main/java/com/seu/fmy/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,31 @@
+package com.seu.fmy.shortlink.admin.config;
+
+/**
+ * @ClassName：MyMetaObjectHandler
+ * @Author: fmy
+ * @Date: 2025/10/9 17:27
+ * @Description:
+ */
+
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
