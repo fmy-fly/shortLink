9d822296df1c29afcbd8991f910d53eb1b8cbe9d
“fmy-fly” <1791477899@qq.com>
2025-11-11 11:38:55 +0800

    开发短链接分页查询功能

--- project/src/main/java/com/seu/fmy/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/seu/fmy/shortlink/project/controller/ShortLinkController.java
@@ -1,11 +1,20 @@
 package com.seu.fmy.shortlink.project.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.seu.fmy.shortlink.project.common.convention.result.Result;
 import com.seu.fmy.shortlink.project.common.convention.result.Results;
+import com.seu.fmy.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.seu.fmy.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.seu.fmy.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.seu.fmy.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+import com.seu.fmy.shortlink.project.service.ShortLinkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * 短链接控制层
@@ -22,5 +31,13 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return Results.success(shortLinkService.createShortLink(requestParam));
+    }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return Results.success(shortLinkService.pageShortLink(requestParam));
     }
 }
--- project/src/main/java/com/seu/fmy/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/seu/fmy/shortlink/project/service/ShortLinkService.java
@@ -1,8 +1,13 @@
 package com.seu.fmy.shortlink.project.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.seu.fmy.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.seu.fmy.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.seu.fmy.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 
 public interface ShortLinkService {
     ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
+    IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam);
 }
