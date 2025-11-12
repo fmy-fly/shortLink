b1142a6af0d61af48672fe13de9b32c014d1c00d
“fmy-fly” <1791477899@qq.com>
2025-11-12 21:23:01 +0800

    修复用户创建分组时用户上下文获取不到用户信息

--- admin/src/main/java/com/seu/fmy/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/seu/fmy/shortlink/admin/service/GroupService.java
@@ -21,6 +21,13 @@ public interface GroupService extends IService<GroupDO> {
      */
     void saveGroup(String groupName);
 
+    /**
+     * 新增短链接分组
+     *
+     * @param username  用户名
+     * @param groupName 短链接分组名
+     */
+    void saveGroup(String username, String groupName);
 
     /**
      * 查询用户短链接分组集合
@@ -28,12 +35,14 @@ public interface GroupService extends IService<GroupDO> {
      * @return 用户短链接分组集合
      */
     List<ShortLinkGroupRespDTO> listGroup();
+
     /**
      * 修改短链接分组
      *
      * @param requestParam 修改链接分组参数
      */
     void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
+
     /**
      * 删除短链接分组
      *
--- admin/src/main/java/com/seu/fmy/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/seu/fmy/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+
 /**
  * 短链接分组接口实现层
  */
@@ -39,14 +40,19 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String groupName) {
+        saveGroup(UserContext.getUsername(), groupName);
+    }
+
+    @Override
+    public void saveGroup(String username, String groupName) {
         String gid;
         do {
             gid = RandomGenerator.generateRandom();
-        } while (!hasGid(gid));
+        } while (!hasGid(username, gid));
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
                 .sortOrder(0)
-                .username(UserContext.getUsername())
+                .username(username)
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
@@ -107,10 +113,10 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         });
     }
 
-    private boolean hasGid(String gid) {
+    private boolean hasGid(String username, String gid) {
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
-                .eq(GroupDO::getUsername, UserContext.getUsername());
+                .eq(GroupDO::getUsername, Optional.ofNullable(username).orElse(UserContext.getUsername()));
         GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
         return hasGroupFlag == null;
     }
