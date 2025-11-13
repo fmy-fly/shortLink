914751e33f336f08b4939aaa63437aa1dc7d36cc
“fmy-fly” <1791477899@qq.com>
2025-11-13 21:19:24 +0800

    短链接后管开发短链接修改功能

--- admin/src/main/java/com/seu/fmy/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/seu/fmy/shortlink/admin/controller/ShortLinkController.java
@@ -2,15 +2,15 @@ package com.seu.fmy.shortlink.admin.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.seu.fmy.shortlink.admin.common.result.Result;
+import com.seu.fmy.shortlink.admin.common.result.Results;
 import com.seu.fmy.shortlink.admin.remote.ShortLinkRemoteService;
 import com.seu.fmy.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.seu.fmy.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.seu.fmy.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.seu.fmy.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.seu.fmy.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * 短链接后管控制层
  */
@@ -29,6 +29,15 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/admin/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PutMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkRemoteService.updateShortLink(requestParam);
+        return Results.success();
     }
 
     /**
