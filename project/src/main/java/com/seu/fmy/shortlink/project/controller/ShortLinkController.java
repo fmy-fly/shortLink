e1931e73180092c8ff3cda7115aa5f119cd7f3a6
“fmy-fly” <1791477899@qq.com>
2025-11-13 21:08:26 +0800

    短链接服务开发修改功能

--- project/src/main/java/com/seu/fmy/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/seu/fmy/shortlink/project/controller/ShortLinkController.java
@@ -6,6 +6,7 @@ import com.seu.fmy.shortlink.project.common.convention.result.Result;
 import com.seu.fmy.shortlink.project.common.convention.result.Results;
 import com.seu.fmy.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.seu.fmy.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.seu.fmy.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkPageRespDTO;
@@ -30,6 +31,12 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return Results.success(shortLinkService.createShortLink(requestParam));
+    }
+
+    @PutMapping("/api/short-link/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkService.updateShortLink(requestParam);
+        return Results.success();
     }
 
     /**
--- project/src/main/java/com/seu/fmy/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/seu/fmy/shortlink/project/service/ShortLinkService.java
@@ -5,12 +5,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.seu.fmy.shortlink.project.dao.entity.ShortLinkDO;
 import com.seu.fmy.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.seu.fmy.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.seu.fmy.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.seu.fmy.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.List;
-
 /**
  * 短链接接口层
  */
@@ -23,6 +23,13 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
      * @return 短链接创建信息
      */
     ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
+    /**
+     * 修改短链接
+     *
+     * @param requestParam 修改短链接请求参数
+     */
+    void updateShortLink(ShortLinkUpdateReqDTO requestParam);
 
     /**
      * 分页查询短链接
